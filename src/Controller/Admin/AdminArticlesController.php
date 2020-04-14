5850e1780cd34a4dc1ed27599d0acbfbb02ddbcc
keyserwood <elouan.raymond@etu.emse.fr>
2020-04-14 14:29:48 +0200

    Delete & Add are Fine

--- src/Controller/Admin/AdminArticlesController.php
+++ src/Controller/Admin/AdminArticlesController.php
@@ -23,21 +23,45 @@ class AdminArticlesController extends AbstractController
         ]);
     }
     /**
-     * @Route("/admin/article/{id}", name="modifArticles")
+     * @Route("/admin/aliment/creation",name="creationArticle")
+     * @Route("/admin/article/{id}", name="modifArticles",methods="GET")
      */
-    public function modifArticle(Articles $article,Request $request,ManagerRegistry $managerRegistry)
+    public function modifArticle(Articles $article = null,Request $request,ManagerRegistry $managerRegistry)
     {
+        if (!$article)
+        {
+            $article = new Articles();
+        }
         $form = $this->createForm(ArticlesType::class,$article);
         $form->handleRequest($request);
         if($form->isSubmitted()&&$form->isValid())
         {
+            $modif = $article->getId() !== null;
             $em = $managerRegistry->getManager();
             $em->persist($article);
             $em->flush();
+            $this->addFlash("success", ($modif)? "La modification a été effectuée" : "L'ajout a été effectué");
             return $this->redirectToRoute("admin_articles");
         }
         return $this->render('admin/admin_articles/modifAjout.html.twig', [
             'article' => $article,"form" => $form->createView()
         ]);
+    }
+    /**
+     * @Route("/admin/article/{id}", name="suppressionArticle",methods="SUP")
+     */
+    public function suppArticle(Articles $article, Request $request, ManagerRegistry $managerRegistry)
+    {
+        if($this->isCsrfTokenValid("SUP".$article->getId(),$request->get('_token')))
+        {
+            $em = $managerRegistry->getManager();
+            $em->remove($article);
+            $em->flush();
+            $this->addFlash("success","La suppression a été effectuée");
+            return $this->redirectToRoute("admin_articles");
+
+        }
+
+
     }
 }
