e9e55c9d5ccbf0fa4562d4fefbebf6bb5361a1b5
keyserwood <elouan.raymond@etu.emse.fr>
2020-04-14 11:34:00 +0200

    Form added

--- src/Controller/Admin/AdminArticlesController.php
+++ src/Controller/Admin/AdminArticlesController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Articles;
+use App\Form\ArticlesType;
 use App\Repository\ArticlesRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminArticlesController extends AbstractController
@@ -16,6 +20,24 @@ class AdminArticlesController extends AbstractController
         $articles = $repository->findAll();
         return $this->render('admin/admin_articles/adminArticles.html.twig', [
             'articles' => $articles
+        ]);
+    }
+    /**
+     * @Route("/admin/article/{id}", name="modifArticles")
+     */
+    public function modifArticle(Articles $article,Request $request,ManagerRegistry $managerRegistry)
+    {
+        $form = $this->createForm(ArticlesType::class,$article);
+        $form->handleRequest($request);
+        if($form->isSubmitted()&&$form->isValid())
+        {
+            $em = $managerRegistry->getManager();
+            $em->persist($article);
+            $em->flush();
+            return $this->redirectToRoute("admin_articles");
+        }
+        return $this->render('admin/admin_articles/modifAjout.html.twig', [
+            'article' => $article,"form" => $form->createView()
         ]);
     }
 }
