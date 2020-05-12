cada0cdb29cf417fc40f56982b85fcd3ca730abd
keyserwood <elouan.raymond@etu.emse.fr>
2020-05-12 10:02:31 +0200

    Articles creation by a user ok!

--- src/Controller/Admin/UserArticlesController.php
+++ src/Controller/Admin/UserArticlesController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Articles;
+use App\Form\ArticlesType;
 use App\Repository\ArticlesRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -20,4 +24,46 @@ class UserArticlesController extends AbstractController
             'articles' => $articles,'userId'=>$userId
         ]);
     }
+    /**
+     * @Route("/user/{id}/article/creation",name="creationArticleUser")
+     * @Route("/user/{idu}/article/{ida}", name="modifArticlesUser",methods="GET")
+     */
+    public function modifArticle(Articles $article = null,Request $request,ManagerRegistry $managerRegistry,UserInterface $user)
+    {
+        if (!$article)
+        {
+            $article = new Articles();
+        }
+        $form = $this->createForm(ArticlesType::class,$article);
+        $form->handleRequest($request);
+        if($form->isSubmitted()&&$form->isValid())
+        {
+            $modif = $article->getId() !== null;
+            $em = $managerRegistry->getManager();
+            $article->setAuthor($user);
+            $em->persist($article);
+            $em->flush();
+            $this->addFlash("success", ($modif)? "La modification a été effectuée" : "L'ajout a été effectué");
+            return $this->redirectToRoute("admin_user_articles",array('id'=>$user->getId()));
+        }
+        return $this->render('admin/user_articles/modifAjoutUA.html.twig', [
+            'article' => $article,"form" => $form->createView()
+        ]);
+    }
+    /**
+     * @Route("/user/{idu}/article/{ida}", name="suppressionArticleUser",methods="SUP")
+     */
+    public function suppArticle(Articles $article, Request $request, ManagerRegistry $managerRegistry,UserInterface $user)
+    {
+        if($this->isCsrfTokenValid("SUP".$article->getId(),$request->get('_token')))
+        {
+            $em = $managerRegistry->getManager();
+            $em->remove($article);
+            $em->flush();
+            $this->addFlash("success","La suppression a été effectuée");
+            return $this->redirectToRoute("admin_user_articles",array('id'=>$user->getId()));
+
+        }
+    }
+
 }
