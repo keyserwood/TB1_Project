80c94a85a927dce38b6f08406ef538809c64b7d3
keyserwood <elouan.raymond@etu.emse.fr>
2020-05-19 17:04:07 +0200

    Comments have an admin panel too !

--- src/Controller/CommentairesController.php
+++ src/Controller/CommentairesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Articles;
 use App\Entity\Commentaires;
 use App\Form\CommentairesType;
+use App\Repository\CommentairesRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,4 +41,31 @@ class CommentairesController extends AbstractController
             'commentaires' => $commentaires,"form" => $form->createView(),"modif"=>$modif
         ]);
     }
+    /**
+     * @Route("/articles/article/{id}/commentaires", name="suppressionComment",methods="SUP")
+     */
+    public function suppComment(Commentaires $commentaires, Request $request, ManagerRegistry $managerRegistry, Articles $articles)
+    {
+        if($this->isCsrfTokenValid("SUP".$commentaires->getId(),$request->get('_token')))
+        {
+            $em = $managerRegistry->getManager();
+            $em->remove($commentaires);
+            $em->flush();
+            $this->addFlash("success","La suppression a été effectuée");
+            return $this->redirectToRoute("afficherArticle",array('id'=>$articles->getId()));
+
+        }
+    }
+    /**
+     * @Route("/admin/commentaires", name="admin_comment")
+     */
+    public function index(CommentairesRepository $repository)
+    {
+        $commentaires =$repository->findAll();
+        return $this->render('admin/admin_commentaires/adminComment.html.twig', [
+            'commentaires' => $commentaires,
+            ]);
+    }
+
+
 }
