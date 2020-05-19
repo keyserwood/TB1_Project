34a1b7a8defc9b1a3aa80e51f4ea65aacbe63c33
keyserwood <elouan.raymond@etu.emse.fr>
2020-05-19 10:55:14 +0200

    Editing Admin Users

--- src/Controller/Admin/AdminUsersController.php
+++ src/Controller/Admin/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Form\UsersManagerType;
 use App\Repository\UsersRepository;
 use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,23 +23,40 @@ class AdminUsersController extends AbstractController
             'users' => $users,
         ]);
     }
-//    /**
-//     * @Route("/admin/users/creation",name="creationUsers")
-//     * @Route("/admin/users/{id}", name="modifUsers",methods={"GET","POST"})
-//     */
-//    public function modifUsers(User$user,Request $request,ManagerRegistry $managerRegistry,UserInterface $user)
-//    {
-//        if (!user)
-//        {
-//            $user = new User();
-//        }
-//
-//
-//    }
+    /**
+     * @Route("/admin/users/{id}", name="modifUsers",methods={"GET","POST"})
+     */
+    public function modifUsers(User $user,Request $request,ManagerRegistry $managerRegistry)
+    {
 
-
-
-
+        $form  = $this->createForm(UsersManagerType::class,$user);
+        $form->handleRequest($user);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $managerRegistry->getManager();
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash("success", "La modification a été effectuée");
+            return $this->redirectToRoute("admin_users");
+        }
+        return $this->render('admin/admin_users/modifUsers.html.twig', [
+            'user' => $user,"form" => $form->createView()
+        ]);
+    }
+    /**
+     * @Route("/admin/users/{id}", name="suppressionUser",methods="SUP")
+     */
+    public function suppUser(User $user, Request $request, ManagerRegistry $managerRegistry)
+    {
+        if($this->isCsrfTokenValid("SUP".$user->getId(),$request->get('_token')))
+        {
+            $em = $managerRegistry->getManager();
+            $em->remove($user);
+            $em->flush();
+            $this->addFlash("success","La suppression a été effectuée");
+            return $this->redirectToRoute("admin_users");
+        }
+    }
 
 
 }
