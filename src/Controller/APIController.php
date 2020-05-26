1dc75492325231e6cf579f800b59e044251ee6b6
keyserwood <elouan.raymond@etu.emse.fr>
2020-05-26 08:57:28 +0200

    API Controller on its way

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -2,16 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class APIController extends AbstractController
 {
     /**
-     * @Route("/a/p/i", name="a_p_i")
+     * @Route("/api", name="api",methods={"GET})
      */
-    public function index()
+    public function liste(ArticlesRepository $articlesRepository)
     {
+        // Récupérer la liste
+        $articles = $articlesRepository->findAll();
+        // Encodage en JSON
+        $encoders = [new JsonEncoder()];
+        // Instanciation du normaliseur
+        $normalizers = [new ObjectNormalizer()];
+        // Instanciation du convertisseur
+        $serializer = new Serializer($normalizers,$encoders);
+        // Convertir en JSON
+        $jsonContent = $serializer->serialize($articles,'json',['circular_reference_handler'=>function($object){return $object->getId();}]);
         return $this->render('api/index.html.twig', [
             'controller_name' => 'APIController',
         ]);
